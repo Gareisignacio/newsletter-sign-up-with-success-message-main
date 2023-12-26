b45f093a3732284b728fa2de0298e5d07f446416
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2023-12-26 18:37:25 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,31 @@
+const subsBtn = document.querySelector("#sub-btn");
+
+const dissBtn = document.querySelector("#diss-btn");
+
+
+
+subsBtn.addEventListener("click", ()=>{
+   const emailInput =document.querySelector("#email");
+   let emailValue = emailInput.value;
+
+   let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+   let itsValid = expReg.test(emailValue);
+      
+   if(itsValid === true){
+      const frontCont = document.querySelector(".front-container");
+      const backCont = document.querySelector(".back-container");
+   
+      console.log("valido")
+         // frontCont.style = "display: none"
+         // backCont.style = "display: block"
+      alert("valido")
+   } else {
+      let invalidEmail = document.querySelector(".invalid-email");
+      invalidEmail.classList.add("visible")
+      let inputBox = document.querySelector("#email");
+      inputBox.classList.add("invalid-box")
+   }
+});
+
+
+
